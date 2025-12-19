6ca5ead8c7c88582defd1c8a1ebf91156b0788c4
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2025-12-19 12:33:23 +0330

    Complete models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import Blog,Article
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("name","anonymos_bloger","bloger")
+    list_editable = ("anonymos_bloger","bloger")
+    list_filter = ("anonymos_bloger","bloger")
+    search_fields = ("name","description")
+    
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "blog",
+        "is_active",
+        "is_verify",
+        "is_pin",
+    )
+    list_editable = (
+        "is_active",
+        "is_verify",
+        "is_pin",
+    )
+    list_filter = (
+        "is_active",
+        "is_verify",
+        "is_pin",
+        "blog",
+    )
+    search_fields = (
+        "title",
+        "summary",
+        "description",
+        "author__username",
+        "author__email",
+    )
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+    autocomplete_fields = (
+        "author",
+        "blog",
+    )
+
+    ordering = (
+        "-id",
+    )
+    fieldsets = (
+        ("اطلاعات اصلی", {
+            "fields": ("title", "slug", "summary", "description", "thumbnail"),
+        }),
+        ("ارتباطات", {
+            "fields": ("author", "blog"),
+        }),
+        ("وضعیت مقاله", {
+            "fields": ("is_active", "is_verify", "is_pin"),
+        }),
+    )
--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 
-"""
-وقت مایگریشن نیست!
-"""
 class Blog(models.Model):
     name = models.CharField(verbose_name="نام وبلاگ",max_length=110)
     description = models.TextField(verbose_name="توضیحات وبلاگ",blank=True,null=True)
-    anonymons_bloger = models.BooleanField(verbose_name="ناشناس کردن نویسنده",default=False,help_text="اگر فعال شود نام نویسنده در بلاگ نمایش داده نمی شود")
-    
+    anonymos_bloger = models.BooleanField(verbose_name="ناشناس کردن نویسنده",default=False,help_text="اگر فعال شود نام نویسنده در بلاگ نمایش داده نمی شود")
+    bloger = models.ForeignKey("accounts.CustomUser",on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    
+
+    class Meta:
+        verbose_name = "وبلاگ"    
+        verbose_name_plural = "وبلاگ ها"    
 
 class Article(models.Model):
     title = models.CharField(verbose_name="موضوع")
+    summary = models.TextField(verbose_name="خلاصه")
     description = models.TextField(verbose_name="توضیحات",blank=True,null=True)
+    slug = models.SlugField(verbose_name="شناسه")
+    thumbnail = models.ImageField("تصویر بندانگشتی",unique=True)
+    author = models.ForeignKey("accounts.CustomUser",on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name="فعال",default=True)
+    is_verify = models.BooleanField(verbose_name="مورد تائید",default=True)
+    is_pin = models.BooleanField(verbose_name="ویژه",default=False)
+
+    class Meta:
+        verbose_name = "مقاله"    
+        verbose_name_plural = "مقالات"    
+
+    def __str__(self):
+        return self.title
